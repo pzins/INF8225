3a1dae3d32da097235da1840b69d419caacf739e
Pierre <zins.pierre@gmail.com>
2017-01-29 10:45:56 -0500

    clean + comments

--- TP1/main.py
+++ TP1/main.py
@@ -1,7 +1,7 @@
 import re
 
 class Node:
-	'Class representing a node in a graph of factors (bayes net)'
+	'Class representing a node in a factor graph (bayes net)'
 
 	def __init__(self, name):
 		self.name = name
@@ -11,6 +11,7 @@ class Node:
 	def addNeighbours(self, neighbours):
 		for i in neighbours:
 			self.neighbours.append(i)
+		# sort neighbours according to their number of neighbours
 		self.neighbours.sort(key = lambda x: len(x.neighbours))
 
 
@@ -26,56 +27,66 @@ class Node:
 		return ""		
 
 class Variable(Node):
-	'Class representing a variable in a graph of factors (bayes net)'
+	'Class representing a variable in a factor graph (bayes net)'
 
 	def __init__(self, name):
 		super().__init__(name)
+		self.root = False
 
 	def getMessage(self, caller, observed):
 		self.visited = True
-		if len(self.neighbours) == 1 and self.name != "x3":
+		if len(self.neighbours) == 1 and not self.root:
 			return "1"
 		st = ""
 		for i in self.neighbours:
 			if not i.visited:
-				st += i.getMessage(self.name, observed) + " "
+				st += i.getMessage(self.name, observed)
 		return st
 
 class Function(Node):
-	'Class representing a function in a graph of factor (bayes net)'
+	'Class representing a function in a factor graph (bayes net)'
 
 	def __init__(self, name, proba):
 		super().__init__(name)
 		self.proba = proba
+		
+		# extract all variables from the function (x1, x2, ...)
 		tmp=proba
+		# we remove some character
 		for i in ["P","(",")",","," ","|"]:
 			tmp = tmp.replace(i, "")
 		self.proba_args = re.findall('..', tmp)
 
 
 
-			
-
 	def getMessage(self, caller, observed):
 		self.visited = True
 		# if function node is a leaf => no sum
-		if len(self.neighbours) == 1 and self.name != "x3":
+		if len(self.neighbours) == 1:
 			return self.proba
 		st = ""
 		for i in self.neighbours:
+			#one node should not be observed and not considered twice
 			if not i.visited and i not in observed:
+				# marginalisation : if other variables (x1, x2, ...) are in the function, we compute a sum over these variables
 				if len(self.proba_args)>1:
 					somme_str = "SOMME_"
 					for j in self.proba_args:
+						# no sum over the current variable
 						if j != caller:
 							somme_str += "_"+j
-				else:
-					somme_str = ""
-
-				st += somme_str + " " + self.proba + " " + i.getMessage(self.name, observed) + " "
+				st += somme_str + " " + self.proba + " " + i.getMessage(self.name, observed)
 		return st
 
+def getProbability(node, observed):
+	# we specify this node as the root 
+	node.root = True
+	res = ""
+	for i in node.neighbours:
+		res += "[" + node.getMessage(node.name, observed) + "] "
+	return res
 
+# declaration of the factor graph
 fa = Function("fa", "P(x1)")
 fb = Function("fb", "P(x2)")
 fc = Function("fc", "P(x3|x1)")
@@ -88,8 +99,7 @@ x3 = Variable("x3")
 x4 = Variable("x4")
 x5 = Variable("x5")
 
-mainNode = x3
-
+# declaration of the edges
 x5.addNeighbours([fe])
 fe.addNeighbours([x2, x5])
 fb.addNeighbours([x2])
@@ -101,4 +111,5 @@ fa.addNeighbours([x1])
 fc.addNeighbours([x1, x3])
 x3.addNeighbours([fc])
 
-print(x3.getMessage(x3.name, [x4]))
+# compute probability
+print(getProbability(x3, [x4]))
