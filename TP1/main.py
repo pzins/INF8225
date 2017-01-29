1215b61652c2a3d0fdb300f0562139488f8c70e5
Pierre <zins.pierre@gmail.com>
2017-01-28 23:12:54 -0500

    add possibility to observe variables

--- TP1/main.py
+++ TP1/main.py
@@ -1,11 +1,11 @@
+import re
 
 class Node:
 	'Class representing a node in a graph of factors (bayes net)'
 
-	def __init__(self, name, proba):
+	def __init__(self, name):
 		self.name = name
 		self.neighbours = []
-		self.proba = proba
 		self.visited = False
 
 	def addNeighbours(self, neighbours):
@@ -28,59 +28,65 @@ class Node:
 class Variable(Node):
 	'Class representing a variable in a graph of factors (bayes net)'
 
-	def __init__(self, name, proba):
-		super().__init__(name, proba)
+	def __init__(self, name):
+		super().__init__(name)
 
-	def getMessage(self):
+	def getMessage(self, caller, observed):
 		self.visited = True
 		if len(self.neighbours) == 1 and self.name != "x3":
-			return self.proba
+			return "1"
 		st = ""
 		for i in self.neighbours:
 			if not i.visited:
-				st += i.getMessage() + " "
+				st += i.getMessage(self.name, observed) + " "
 		return st
 
 class Function(Node):
 	'Class representing a function in a graph of factor (bayes net)'
 
 	def __init__(self, name, proba):
-		super().__init__(name, proba)
-	
-	def getMessage(self):
+		super().__init__(name)
+		self.proba = proba
+		tmp=proba
+		for i in ["P","(",")",","," ","|"]:
+			tmp = tmp.replace(i, "")
+		self.proba_args = re.findall('..', tmp)
+
+
+
+			
+
+	def getMessage(self, caller, observed):
 		self.visited = True
+		# if function node is a leaf => no sum
 		if len(self.neighbours) == 1 and self.name != "x3":
 			return self.proba
 		st = ""
 		for i in self.neighbours:
-			if not i.visited:
-				st += self.proba + " " + i.getMessage() + " "
+			if not i.visited and i not in observed:
+				if len(self.proba_args)>1:
+					somme_str = "SOMME_"
+					for j in self.proba_args:
+						if j != caller:
+							somme_str += "_"+j
+				else:
+					somme_str = ""
+
+				st += somme_str + " " + self.proba + " " + i.getMessage(self.name, observed) + " "
 		return st
 
-class Message:
-	'Class representing a message'
 
-	def __init__(self, origin, dest, typeM):
-		self.origin = origin
-		self.dest = dest
-		self.typeM = typeM
+fa = Function("fa", "P(x1)")
+fb = Function("fb", "P(x2)")
+fc = Function("fc", "P(x3|x1)")
+fd = Function("fd", "P(x4|x1, x2)")
+fe = Function("fe", "P(x5|x2)")
 
-	def __str__(self):
-		return origin.name+"->"+dest.name
-
-
-
-fa = Function("fa", "SOMME_ P(x1)")
-fb = Function("fb", "SOMME_ P(x2)")
-fc = Function("fc", "SOMME_ P(x3|x1)")
-fd = Function("fd", "SOMME_ P(x4|x1, x2)")
-fe = Function("fe", "SOMME_ P(x5|x2)")
-
-x1 = Variable("x1", "")
-x2 = Variable("x2", "")
-x3 = Variable("x3", "")
-x4 = Variable("x4", "")
-x5 = Variable("x5", "1")
+x1 = Variable("x1")
+x2 = Variable("x2")
+x3 = Variable("x3")
+x4 = Variable("x4")
+x5 = Variable("x5")
 
 mainNode = x3
 
@@ -95,4 +101,4 @@ fa.addNeighbours([x1])
 fc.addNeighbours([x1, x3])
 x3.addNeighbours([fc])
 
-print(x3.getMessage())
+print(x3.getMessage(x3.name, [x4]))
