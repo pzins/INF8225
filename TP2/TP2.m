5f569a15b13389c4497fd862acbb15214be20c2f
Pierre Zins <Pierre Zins>
2017-02-07 20:36:38 -0500

    save

--- TP2/TP2.m
+++ TP2/TP2.m
@@ -25,20 +25,28 @@ for k=1:4
 end
 Z = sum(P_Y);
 P_Y_sachant_X = P_Y ./ [Z;Z;Z;Z]
-Prob_y_sachant_x=P_Y;
-for k=1:length(Z)
-    Prob_y_sachant_x(:,k)=Prob_y_sachant_x(:,k)/Z(1,k);
-end
-min(min(Prob_y_sachant_x == P_Y_sachant_X))
-% sum(sum(P_Y))
+% Prob_y_sachant_x=P_Y;
+% for k=1:length(Z)
+%     Prob_y_sachant_x(:,k)=Prob_y_sachant_x(:,k)/Z(1,k);
+% end
 
-% sum(sum(exp(Theta*X)))
+% Z = sum(exp(possibleY * Theta * X))
+a=Z;
+b=sum(exp(possibleY * Theta * X));
+min(min(a==b));
+% ----------------
+
+sum(sum(log(Y*P_Y_sachant_X))) %test mais en faite pas encore ca
+a = sum(sum(P_Y_sachant_X));
+b = sum(sum(((Y * Theta) .* X')'));
 
 
 
-logVraisemblance = sum(sum(((Y * Theta) .* X')') - log(sum(exp(possibleY * Theta * X))));
+
+
+logVraisemblance = sum(sum(((Y * Theta) .* X')') - log(sum(exp(possibleY * Theta * X))))
 logVraisemblance_ = sum(sum(((Y * Theta) .* X')') - log(sum(exp(Theta*X))));
-
+break
 left = Y' * X';
 
 Z = sum(exp(Theta*X));
