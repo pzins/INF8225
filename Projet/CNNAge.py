d4e784d9b1406a337086c9ad7ceb5d1228845ec1
Pierre <zins.pierre@gmail.com>
2017-04-05 22:13:48 -0400

    update and save work

---
+++ Projet/CNNAge.py
@@ -0,0 +1,112 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+from numpy import random
+from PIL import Image
+import glob
+from keras import optimizers
+from keras.models import load_model
+
+
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+#CNN
+#------------------------------------------------------
+
+"""
+index = np.arange(x.shape[0])
+np.random.shuffle(index)
+
+sizeTrain = int(x.shape[0] * 0.6)
+sizeValTest = int(x.shape[0] * 0.2)
+
+x_train = x[index[:sizeTrain]]
+x_valid = x[index[sizeTrain:sizeTrain+sizeValTest]]
+x_test = x[index[sizeTrain+sizeValTest:sizeTrain+sizeValTest*2]]
+y_train = y[index[:sizeTrain]]
+y_valid = y[index[sizeTrain:sizeTrain+sizeValTest]]
+y_test = y[index[sizeTrain+sizeValTest:sizeTrain+sizeValTest*2]]
+"""
+x_set = np.array([]).reshape(0, 50, 50, 1)
+y_set = np.array([]).reshape(0, 6)
+for it in range(6):
+    x_tmp = np.load("data2/xtrain_50_" + str(it) + ".dat")
+    y_tmp = np.load("data2/ytrain_50_" + str(it) + ".dat")
+    x_set = np.append(x_set, x_tmp, axis=0)
+    y_set = np.append(y_set, y_tmp, axis=0)
+
+trainSize = int(x_set.shape[0] * 0.7)
+
+x_train = x_set[:trainSize]
+y_train = y_set[:trainSize]
+x_test = x_set[trainSize:]
+y_test = y_set[trainSize:]
+
+epochs = 50
+batch_size = 32
+num_classes = 6
+input_shape = (50, 50,1)
+
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# model = load_model("keras_model.h5")
+model = Sequential()
+model.add(Conv2D(96, kernel_size=(7, 7),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(256, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(384, (3, 3), activation='relu'))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# opt = optimizers.Adam(lr=0.01)
+opt = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+
+model.compile(loss="categorical_crossentropy",#keras.losses.categorical_crossentropy,
+              optimizer=opt,
+              metrics=['accuracy'])
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+score = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+model.save('keras_model_age.h5')
+
+
+pred = model.predict(x_test)
+print(pred)
+# for i in range(len(pred)):
+	# print(str(pred[i]) + " => " + str(y_test[i]))
--- Projet/faces.py
+++ Projet/faces.py
@@ -21,12 +21,16 @@ from datetime import *
 
 import scipy.misc
 
-def getAge(matlabDate):
-	matlabDate = np.asscalar(matlabDate)
-	python_datetime = datetime.fromordinal(int(matlabDate)) + timedelta(days=matlabDate%1) - timedelta(days = 366)
-	return datetime.now().year - python_datetime.year
+def getAge(dob, photo_date):
+	dob = np.asscalar(dob)
+	python_datetime = datetime.fromordinal(int(dob)) + timedelta(days=dob%1) - timedelta(days = 366)
+	res = photo_date - python_datetime.year
+	if res < 10 or res > 100:
+		res = -1
+	return res
 
 # group age into categories
+# -1 => error
 # 0 => <20
 # 1 => 20-30
 # 2 => 30-40
@@ -34,7 +38,9 @@ def getAge(matlabDate):
 # 4 => 50-60
 # 5 => >60
 def getAgeCategory(age):
-	if age<20:
+	if age < 10 or age > 100:
+		return -1
+	elif age<20:
 		return 0
 	elif age >= 20 and age < 30:
 		return 1
@@ -51,103 +57,110 @@ def getAgeCategory(age):
 data = sio.loadmat("wiki.mat")
 # print(data.shape)
 d = data["wiki"]
-taille = 10000
+taille_img_out = 32
+nb_categories = 2
 
 a = np.array(d[0][0][2][:][0])
 
-x_train = np.array([np.array(Image.open("/home/pierre/Downloads/wiki_crop/"+fname[0])) for fname in a[:taille]])
+dataset_size = a.shape[0]
+a = a[:60000]
+print(dataset_size)
+
+batchSize = 10000
+
+for it in range(int(dataset_size/batchSize)):
+
+	x_train = np.array([np.array(cv2.imread("/home/pierre/Downloads/wiki_crop/"+fname[0], 1)) for fname in a[it*batchSize:(it+1)*batchSize]])
+
+	b = np.array(d[0][0][3][:][0][it*batchSize:(it+1)*batchSize])
+	"""
+	dob = np.array(d[0][0][0][:][0][it*batchSize:(it+1)*batchSize])
+	photo_date = np.array(d[0][0][1][:][0][it*batchSize:(it+1)*batchSize])
+
+	for i in range(len(dob)):
+		# dob[i] = getAgeCategory(getAge(dob[i], photo_date[i]))
+		dob[i] = getAge(dob[i], photo_date[i])
+	b = dob
+	"""
+
+	# c = np.array(d[0][0][5][:][0][:taille])
+	# e = np.array(d[0][0][2][:][0][:taille])
+	# f = np.array(d[0][0][4][:][0][:taille])
+	# g = np.array(d[0][0][6][:][0][:taille])
 
 
-b = np.array(d[0][0][3][:][0][:taille])
-"""
-b = np.array(d[0][0][0][:][0][:taille])
+	casc = "haarcascade_frontalface_default.xml"
+	faceCascade = cv2.CascadeClassifier(casc)
 
-for i in range(len(b)):
-	b[i] = getAgeCategory(getAge(b[i]))
-"""
+	real_set = []
+	label = []
+	counter = 0
+	for img in x_train:
+		# img = cv2.imread(image)
+		
+		# if len(img.shape) == 3:
+			# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		if len(img.shape) == 2:
+			counter += 1
+			continue
+		# img =cv2.equalizeHist(img )
+		faces = faceCascade.detectMultiScale(
+			img,
+			scaleFactor=1.1,
+			minNeighbors=10,
+			minSize=(30, 30),
+			flags=cv2.CASCADE_SCALE_IMAGE)
+		test = 0
+		for (x,y,w,h) in faces:
+			# print(w,h)
+			# w = 46
+			# h = 56
+			# cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+			
+			#enlarge bounding box
+			y -= int(h*0.25)
+			x -= int(w*0.25)
+			h += int(h*0.25)
+			w += int(w*0.25)
 
-# c = np.array(d[0][0][5][:][0][:taille])
-# e = np.array(d[0][0][2][:][0][:taille])
-# f = np.array(d[0][0][4][:][0][:taille])
-# g = np.array(d[0][0][6][:][0][:taille])
+			im = img[y:y+h,x:x+w]
+			
+			if im.shape[0] < taille_img_out or im.shape[1] < taille_img_out:
+				break
 
+			im = cv2.resize(im, (taille_img_out, taille_img_out))
+			real_set.append(im)
+			test = 1
+			print(counter)
 
-"""
-#version avec face location du dataset mais coo strange
-counter = 0
-for img in x_train:
-	coo = c[counter][0]
-	coo = coo.astype(int)
-	x = coo[0]
-	y = coo[1]
-	x2 = coo[2]
-	y2 = coo[3]
-	print(coo)
-	cv2.rectangle(img, (x,y), (x2,y2), (0,255,0), 2)
+			# cv2.imshow("Face found", im)
+			# cv2.waitKey(0)
 
-	# im = img[coo[1]:coo[3], coo[0]:coo[2]]
-
-	cv2.imshow("Face found", img)
-	cv2.waitKey(0)	
-	counter += 1
-"""
-casc = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(casc)
-
-real_set = []
-label = []
-counter = 0
-for img in x_train:
-	# print(counter)
-	# img = cv2.imread(image)
-
-	if len(img.shape) == 3:
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img =cv2.equalizeHist(img )
-	
-	faces = faceCascade.detectMultiScale(
-		img,
-		scaleFactor=1.1,
-		minNeighbors=10,
-		minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
-	test = 0
-	for (x,y,w,h) in faces:
-		# print(w,h)
-		# w = 46
-		# h = 56
-		cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-		im = img[y:y+h,x:x+w]
-		# print(im.shape)
-		if im.shape[0] < 128 or im.shape[1] < 128:
+			# save img file	
+			# if counter < 250:	
+				# name = "test/" + str(counter) + "_" + str(int(b[counter])) + '.jpg'
+				# scipy.misc.imsave(name, im)
 			break
-		im = cv2.resize(im, (128, 128))
-		real_set.append(im)
-		test = 1
-		print(counter)
-		# print(b[counter])
-		# cv2.imshow("Face found", im)
-		# cv2.waitKey(0)
+		if test == 1:
+			if np.isnan(b[counter]) or b[counter] == -1:
+				real_set.pop()
+			else:
+				label.append(b[counter])
 
-		# save img file		
-		# name = "test/" + str(counter) + '.jpg'
-		# scipy.misc.imsave(name, im)
-		break
-	if test == 1:
-		if np.isnan(b[counter]):
-			real_set.pop()
-		else:
-			label.append(b[counter])
+		counter += 1
 
-	counter += 1
-	# cv2.destroyAllWindows()
+		# cv2.destroyAllWindows()
 
-x_train = np.asarray(real_set)
-y_train = np.array(label)
-y_train = keras.utils.to_categorical(y_train, 2)
+	x_train = np.asarray(real_set)
+	y_train = np.array(label)
+	y_train = keras.utils.to_categorical(y_train, nb_categories)
+	# x_train = np.expand_dims(x_train, 4)
 
-x_train = np.expand_dims(x_train, 4)
-
-# print(x_train.shape)
-x_train.dump("xtrain_128.dat")
-y_train.dump("ytrain_128.dat")
+	x_train.dump("data1000/32_large/xtrain_" + str(taille_img_out) + "_" + str(it) + ".dat")
+	y_train.dump("data1000/32_large/ytrain_" + str(taille_img_out) + "_" + str(it) + ".dat")
+#data7 => test
+#data6 => size 128 + colored
+#data4 => size 250	
+#data5 => size 128
+#data3 => avc age regression
+#data2 => avc age category
